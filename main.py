a8e921a00027c8ec418b2fefbfcf5b19c1a5747e
Hubert Koszyk <hubertkoszyk@zohomail.eu>
2025-07-31 13:21:38 +0200

    Update main.py
    Added PL Version

--- main.py
+++ main.py
@@ -7,38 +7,38 @@ for line in file:
 choseen_word = random.choice(data)
 i = 1
 chars = []
-print("/////WELCOME IN SIMPLE COUNTRY WORDLE/////")
-print("Guess the word with", len(choseen_word), "letters")
-print("You have 6 tries")
+print("/////WITAJ W PROSTYM WORDLE/////")
+print("ZGADNIJ PAŃSTWO KTÓRE MA", len(choseen_word), "LITER")
+print("MASZ 6 PRÓB")
 bad_chars = []
 chars_in_word = []
 for i in range(1, 6):
-  input_type = input("Guess Word:").capitalize()
+  input_type = input("ZGADNIJ SŁOWO ").capitalize()
   input_type_list = list(input_type)
   if len(input_type) > len(choseen_word) or len(input_type) < len(choseen_word):
-    print("Word is too long or to short")
-    print("This is your", i, "try")
+    print("SŁOWO JEST ZA DŁUGIE LUB ZA KRÓTKIE")
+    print("TO TWOJA", i, "PRÓBA")
     i = i + 1
     continue
   else:
     if input_type == choseen_word:
-      print("Excelent You Win")
+      print("BRAWO WYGRAŁEŚ")
       break
     else:
-      print("Unfortunately, you didn't guess.")
+      print("NIESTETY NIE ZGADŁEŚ")
       same_chars = set(input_type) & set(choseen_word)
       chars.extend(same_chars)
       def sprawdzanie_tablic():
         for i in range(len(input_type_list)):
           if input_type_list[i] == choseen_word[i]:
-            print("Letter", input_type_list[i], "is in right place")
+            print("Litera", input_type_list[i], "jest w dobrym miejscu")
           elif input_type_list[i] in choseen_word:
-            print("Letter", input_type_list[i], "is in word")
+            print("Litera", input_type_list[i], "jest w słowie")
           else:
-            print("Letter", input_type_list[i], "The letter does not appear in the word")
+            print("Litera", input_type_list[i], "nie pojawia się w słowie")
       sprawdzanie_tablic()
-      print("This your", i, "try")
-      print("This word has", len(choseen_word), "letters")
+      print("TO TWOJA", i, "PRÓBA")
+      print("TWOJE SŁOWO MA", len(choseen_word), "LITER")
       i = i + 1
 
-print("End of the game! Hidden Word is:", choseen_word)
+print("KONIEC GRY. UKRYTE SŁOWO TO", choseen_word)
